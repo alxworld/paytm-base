dad74f4f66858fde828f39bbe8b98e0b201b4317
AlexanderS <jsalex@gmail.com>
2024-04-19 19:13:09 +0530

    base frontend setup

---
+++ frontend/src/pages/SendMoney.jsx
@@ -0,0 +1,8 @@
+export default function SendMoney() {
+  return (
+    <div>
+      <h1>SendMoney</h1>
+      <p>Welcome to the sendmoney page</p>
+    </div>
+  )
+}
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <p>Welcome to the signup page</p>
+    </div>
+  )
+}
